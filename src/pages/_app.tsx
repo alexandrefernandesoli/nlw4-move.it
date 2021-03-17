f16f5366375d050a660c20e0574b15981a20eae5
Alexandre <alexandrefernandes_1@hotmail.com>
2021-03-17 08:49:37 -0400

    async googleFonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
+import { GoogleFonts } from 'next-google-fonts'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" />
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
